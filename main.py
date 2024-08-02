ceee414d7a5aa89d4ca6b5386bb86f4d4dd91b22
gstoltman <gstoltman@gmail.com>
2024-08-01 18:29:48 -0700

    added stat dropdown and improved formatting

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ df = pd.read_csv('/home/grant/projects/batting/data/rosters.csv')
 
 app = Dash()
 
+stat_list = ['G','AB','R','H','2B','3B','HR','RBI','BB','IBB','SO','AVG','OBP','SLG']
+
+max_stat = {}
+
+for i in stat_list:
+    max_stat[i] = df[i].max()
+
 # Define the layout
 app.layout = html.Div([
     dcc.Dropdown(
@@ -16,22 +23,59 @@ app.layout = html.Div([
         options=[{'label': name, 'value': name} for name in df['Name'].unique()],
         value=df['Name'].unique()[0]  # Default value
     ),
+    dcc.Dropdown(
+        id='stat-dropdown',
+        options=[{'label': stat, 'value': stat} for stat in stat_list],
+        value=stat_list[0]
+    ),
     dcc.Graph(id='line-chart')
 ])
 
 # Define the callback to update the graph
 @app.callback(
     Output('line-chart', 'figure'),
-    Input('player-dropdown', 'value')
+    [Input('player-dropdown', 'value'),
+    Input('stat-dropdown', 'value')]
 )
-def update_graph(selected_player):
+def update_graph(selected_player, selected_stat):
     # Filter data for the selected player
     filtered_df = df[df['Name'] == selected_player]
+    chosen_stat = selected_stat
 
     # Define different color segments
     colors = ['blue', 'green', 'red', 'purple']
     color_map = {
-        'CHN': 'blue', 'BOS': 'red', 'CHA': 'black', 'SLN': 'red', 'CN5': 'red', 'ANG': 'grey', 'MIA': 'orange', 'TEX': 'red', 'PIT': 'yellow', 'LAN': 'blue', 'SDN': 'yellow', 'WS0': 'red', 'ARI': 'maroon', 'TOR': 'blue', 'TBR': 'darkblue', 'SEA': 'darkblue', 'BAL': 'orange', 'ATL': 'red', 'HOA': 'orange', 'KCA': 'blue', 'PHI': 'red', 'CLE': 'red', 'MIN': 'red', 'NYN': 'orange', 'ML4': 'darkblue', 'OAK': 'green', 'COL': 'purple', 'DET': 'orange', 'NYA': 'black', 'SFN': 'orange', 'CLG': 'red'
+        'CHN': '#0E3386',
+        'BOS': '#BD3039',
+        'CHA': '#27251F',
+        'SLN': '#C41E3A ',
+        'CN5': '#C6011F',
+        'ANG': 'grey',
+        'MIA': '#00A3E0',
+        'TEX': '#003278',
+        'PIT': '#FDB827',
+        'LAN': '#005A9C',
+        'SDN': '#2F241D',
+        'WS0': '#AB0003',
+        'ARI': '#A71930',
+        'TOR': '#134A8E',
+        'TBR': '#092C5C',
+        'SEA': '#0C2C56',
+        'BAL': '#DF4601',
+        'ATL': '#CE1141',
+        'HOA': '#EB6E1F',
+        'KCA': '#004687',
+        'PHI': '#E81828',
+        'CLE': '#E50022',
+        'MIN': '#002B5C',
+        'NYN': '#FF5910',
+        'ML4': '#12284B',
+        'OAK': '#003831',
+        'COL': '#333366',
+        'DET': '#FA4616',
+        'NYA': '#27251F',
+        'SFN': '#FD5A1E',
+        'CLG': '#E50022'
     }
 
     
@@ -44,22 +88,46 @@ def update_graph(selected_player):
         color = color_map.get(team)
         fig.add_trace(go.Scatter(
             x=team_df['year'],
-            y=team_df['AVG'],
-            mode='lines+markers',
+            y=team_df[chosen_stat],
+            mode='lines+markers+text',
             line=dict(color=color, width=7),
             marker=dict(size=20),
+            text=team_df[chosen_stat],
+            textposition='top center',
+            textfont=dict(size=16, family='Arial Black'),
             name=f'{team}'
         ))
 
     # Update layout
     fig.update_layout(
-        paper_bgcolor='darkgrey',
-        plot_bgcolor='darkgrey',
-        title=f'Batting Average (AVG) Over Years for {selected_player}',
-        xaxis_title='Year',
-        yaxis_title='Batting Average',
-        xaxis=dict(range=[2014, 2024], tickmode='linear'),
-        yaxis=dict(range=[.15, .35], tickformat='.3f'),
+        height=800,
+        paper_bgcolor='#626366',
+        plot_bgcolor='#c5c7cd',
+        title=dict(
+            text=f'{selected_player}: {chosen_stat} per Year',
+            font=dict(color='#FFFFFF', size=24),
+            x=0.5,
+            xanchor='center'
+        ),
+        xaxis=dict(
+            range=[2014, 2024],
+            title=dict(
+                text='Year',
+                font=dict(color='#FFFFFF', size=20)
+            ),
+            tickfont=dict(color='#FFFFFF', size=16)
+        ),
+        yaxis=dict(
+            range=[0, max_stat[chosen_stat]],
+            title=dict(
+                text=chosen_stat,
+                font=dict(color='#FFFFFF', size=20)
+            ),
+            tickfont=dict(color='#FFFFFF', size=16)
+        ),
+        legend=dict(
+            font=dict(color='#FFFFFF')
+        ),
         showlegend=True
     )
 
